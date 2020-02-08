625b0b8437bd91c8d15a8502fd336a3a48d812e6
LENA EIVY <applena@gmail.com>
2020-02-08 14:13:53 -0800

    Updates

---
+++ precache-manifest.39291001eaf92194da3ef96865fdf940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a52ecca388bd8ee4b9a4d3ed569d3d",
+    "url": "/lightning-dep/index.html"
+  },
+  {
+    "revision": "a859fe3c061407d32315",
+    "url": "/lightning-dep/static/css/main.90d8c0d3.chunk.css"
+  },
+  {
+    "revision": "09c64e974e23000f047c",
+    "url": "/lightning-dep/static/js/2.96df97d1.chunk.js"
+  },
+  {
+    "revision": "c59f0544f1841cc5018e2e814d4e981b",
+    "url": "/lightning-dep/static/js/2.96df97d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a859fe3c061407d32315",
+    "url": "/lightning-dep/static/js/main.07af11bf.chunk.js"
+  },
+  {
+    "revision": "a1871227a90a9d408983",
+    "url": "/lightning-dep/static/js/runtime-main.b67146e8.js"
+  }
+]);
